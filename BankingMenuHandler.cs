7df0f31f1b56db046c944e3b2d7ab5551376ad0a
Jon Karlsen <karlsen.jonarild@gmail.com>
2019-05-19 11:13:50 +0200

    Implement logging

--- BankingMenuHandler.cs
+++ BankingMenuHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 /// <summary>
 /// Utility class for banking menu operations.
@@ -39,11 +40,62 @@ static class BankingMenuHandler
         }
     }
 
+    private static void WriteResultToFile(AccountHolder currentUser, string operationType)
+    {
+        string logMessage = $"{currentUser.LastName}, {currentUser.FirstName} - #{currentUser.AccountNumber}";
+        logMessage += $"{operationType} - {bankingOptions[operationType]}";
+        logMessage += "\n";
+
+        Console.WriteLine(logMessage);
+
+        if (!File.Exists("log.txt"))
+        {
+            using (StreamWriter sw = File.CreateText("log.txt"))
+            {
+                sw.WriteLine(logMessage);
+            }
+        }
+        else
+        {
+            using (StreamWriter sw = File.AppendText("log.txt"))
+            {
+                sw.WriteLine(logMessage);
+            }
+        }
+    }
+
+    private static void WriteResultToFile(AccountHolder currentUser, string operationType, decimal transactionAmount)
+    {
+        string logMessage = $"{currentUser.LastName}, {currentUser.FirstName} - #{currentUser.AccountNumber}";
+        logMessage += $"{operationType} - {bankingOptions[operationType]} ";
+        logMessage += $"Transaction amount: ${transactionAmount}";
+        logMessage += "\n";
+
+        Console.WriteLine(logMessage);
+
+        if (!File.Exists("log.txt"))
+        {
+            using (StreamWriter sw = File.CreateText("log.txt"))
+            {
+                sw.WriteLine(logMessage);
+            }
+        }
+        else
+        {
+            using (StreamWriter sw = File.AppendText("log.txt"))
+            {
+                sw.WriteLine(logMessage);
+            }
+        }
+    }
+
     private static void HandleCaseI(AccountHolder currentUser)
     {
         Console.WriteLine();
         Console.WriteLine($"Viewing the account of {currentUser.LastName}, {currentUser.FirstName} with account #{currentUser.AccountNumber}");
         Console.WriteLine();
+
+        WriteResultToFile(currentUser, "I");
     }
 
     private static void HandleCaseCB(AccountHolder currentUser)
@@ -53,6 +105,8 @@ static class BankingMenuHandler
         Console.WriteLine("");
         Console.WriteLine($"{currentUser.LastName}, {currentUser.FirstName}'s checking account has a balance of ${currentAccount.ViewBalance()}");
         Console.WriteLine("");
+
+        WriteResultToFile(currentUser, "CB");
     }
 
     private static void HandleCaseCD(AccountHolder currentUser)
@@ -65,6 +119,8 @@ static class BankingMenuHandler
 
         decimal amountToDeposit = Decimal.Parse(Console.ReadLine());
         currentAccount.DepositFunds(amountToDeposit);
+
+        WriteResultToFile(currentUser, "CD", amountToDeposit);
     }
 
     private static void HandleCaseCW(AccountHolder currentUser)
@@ -77,6 +133,8 @@ static class BankingMenuHandler
 
         decimal amountToWithdraw = Decimal.Parse(Console.ReadLine());
         currentAccount.WithdrawFunds(amountToWithdraw);
+
+        WriteResultToFile(currentUser, "CW", amountToWithdraw);
     }
 
     private static void HandleCaseSB(AccountHolder currentUser)
@@ -86,6 +144,8 @@ static class BankingMenuHandler
         Console.WriteLine("");
         Console.WriteLine($"{currentUser.LastName}, {currentUser.FirstName}'s savings account has a balance of ${currentAccount.ViewBalance()}");
         Console.WriteLine("");
+
+        WriteResultToFile(currentUser, "SB");
     }
 
     private static void HandleCaseSD(AccountHolder currentUser)
@@ -98,6 +158,8 @@ static class BankingMenuHandler
 
         decimal amountToDeposit = Decimal.Parse(Console.ReadLine());
         currentAccount.DepositFunds(amountToDeposit);
+
+        WriteResultToFile(currentUser, "SD", amountToDeposit);
     }
 
     private static void HandleCaseSW(AccountHolder currentUser)
@@ -110,6 +172,8 @@ static class BankingMenuHandler
 
         decimal amountToWithdraw = Decimal.Parse(Console.ReadLine());
         currentAccount.WithdrawFunds(amountToWithdraw);
+
+        WriteResultToFile(currentUser, "SW", amountToWithdraw);
     }
 
     private static void HandleCaseX(ref bool displayMenu)
